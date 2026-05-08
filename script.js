e817153799cb99be2eb14b035b4f8b0d8ada5b78
synt-tech <hi@synt.tech>
2026-05-08 21:33:30 +0200

    Update yield

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ function createBackgroundTexts() {
             
             const textElement = document.createElement('div');
             textElement.className = 'background-text';
-            textElement.textContent = '🚀6.67% yield!🚀';
+            textElement.textContent = '🚀6.88% yield!🚀';
             textElement.style.left = `${randomX}px`;
             textElement.style.top = `${randomY}px`;
             textElement.style.transform = `rotate(${rotation}deg)`;
